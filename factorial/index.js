e781d08579e1da7e49c828937ba9456c16b2d726
vittorix <vittorix@gmail.com>
2023-11-06 23:12:44 -0500

    improvement on the text imput to avoid negative, esponential, decimal numbers

--- factorial/index.js
+++ factorial/index.js
@@ -1,5 +1,7 @@
-const source = document.getElementById("source");
+const box = document.getElementById("box");
 const result = document.getElementById("result");
+const error = document.getElementById("error");
+const details = document.getElementById("details");
 let calls = 0;
 
 const factorial = function (n) {
@@ -8,19 +10,52 @@ const factorial = function (n) {
   else return n * factorial(n - 1);
 };
 
+// avoit entering or pasting of exponential numbers, negative numbers and decimal numbers
+// e.g. 1e1 = 10, -2, 3.1 would all be accepted in the input type number
+const beforeinputHandler = function (event) {
+  if (event.data) {
+    const char = event.data.toLowerCase();
+    if (
+      char.indexOf(".") > -1 ||
+      char.indexOf("e") > -1 ||
+      char.indexOf("-") > -1 ||
+      event.data > 170
+    ) {
+      event.preventDefault();
+    }
+  }
+};
+
 const inputHandler = function (number) {
   const n = number.target.value;
   const reg = new RegExp("[0-9]");
+  error.style.visibility = false;
+
   if (reg.test(n) && n > 0 && n < 171) {
     let startTime = window.performance.now();
-    result.innerHTML = "<br> result: " + factorial(n) + "<br> calls: " + calls;
+    let calculaterFactorial = factorial(n);
     let elapsed = window.performance.now() - startTime;
-    result.innerHTML += "<br> milliseconds: " + elapsed;
+
+    result.innerHTML =
+      "<br> " + parseFloat(n + "", 10) + "! = " + calculaterFactorial;
+    details.innerHTML = "<br> recursive calls: " + calls;
+    details.innerHTML += "<br> milliseconds: " + elapsed;
+    error.innerHTML = "";
+
     startTime = 0;
     elapsed = 0;
   } else if (n == "") {
     result.innerHTML = "";
-  } else result.innerHTML = "invalid";
+    details.innerHTML = "";
+    error.innerHTML = "";
+  } else {
+    result.innerHTML = "";
+    error.innerHTML = "<br>please enter a number between 1 and 170";
+    details.innerHTML = "";
+    error.style.visibility = true;
+  }
   calls = 0;
 };
-source.addEventListener("input", inputHandler);
+
+input.addEventListener("beforeinput", beforeinputHandler);
+input.addEventListener("input", inputHandler);
